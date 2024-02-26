418aee3397cb19de272fd17ddc6facb531df8609
itganeshtech <it.ganesh.tech@gmail.com>
2024-02-26 20:55:52 +0530

    FrontEnt Design of Project

---
+++ BulkyWebOne/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyWebOne.Data;
+using BulkyWebOne.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWebOne.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db=db;   
+        }
+
+        public IActionResult Index()
+        {
+           List<Category> objCategoryList=_db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}

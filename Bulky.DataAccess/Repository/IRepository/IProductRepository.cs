4b239b7206a121c7b69b4abe60489f07d7e5fecf
itganeshtech <it.ganesh.tech@gmail.com>
2024-07-13 13:46:05 +0530

    CategoryController with UnitOfWork completed and CategoryController in Admin Area Completed

---
+++ Bulky.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Bulky.Models;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public  interface IProductRepository : IRepository<Product>
+    {
+        public void Update(Product obj);
+        
+    }
+}

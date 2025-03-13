e15df494a1cd9ebd6b7c588a0e928bcdd1ca5904
itganeshtech <it.ganesh.tech@gmail.com>
2025-03-14 03:00:52 +0530

    Section 10 Adding product to shopping cart started.

--- BulkyWebOne/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWebOne/Areas/Customer/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BulkyWebOne.Areas.Customer.Controllers
 {
@@ -23,10 +25,30 @@ namespace BulkyWebOne.Areas.Customer.Controllers
             IEnumerable<Product> productList=_unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
             return View(productList);
         }
+
         public IActionResult Details(int productId)
         {
-            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
-            return View(product);
+            ShoppingCart cart = new()
+            {
+                Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
+                Count = 1,
+                ProductId = productId
+            };
+            return View(cart);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+
+            _unitOfWork.ShoppingCart.Add(shoppingCart);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()

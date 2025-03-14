28267a82d5e01b6f08497f1f5773094672d9e808
itganeshtech <it.ganesh.tech@gmail.com>
2025-03-15 04:37:10 +0530

    Get Order Total Completed

--- Bulky.Models/ShoppingCart.cs
+++ Bulky.Models/ShoppingCart.cs
@@ -24,5 +24,8 @@ namespace Bulky.Models
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
+        
+        [NotMapped]
+        public double Price { get; set; }
     }
 }
--- BulkyWebOne/Areas/Customer/Controllers/CartController.cs
+++ BulkyWebOne/Areas/Customer/Controllers/CartController.cs
@@ -29,7 +29,32 @@ namespace BulkyWebOne.Areas.Customer.Controllers
                 ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
                             includeProperties: "Product")
             };
+
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+            }
+
             return View(ShoppingCartVM);
+        }
+        private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
+        {
+            if (shoppingCart.Count <= 50)
+            {
+                return shoppingCart.Product.Price;
+            }
+            else
+            {
+                if (shoppingCart.Count <= 100)
+                {
+                    return shoppingCart.Product.Price50;
+                }
+                else
+                {
+                    return shoppingCart.Product.Price100;
+                }
+            }
         }
     }
     

87af3128c9a487ddbac1cee8689632d768deeda4
itganeshtech <it.ganesh.tech@gmail.com>
2025-03-18 20:25:39 +0530

    Section 12 Loading OrderDetails Header.

---
+++ BulkyWebOne/Areas/Admin/Views/Order/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebOne.Areas.Admin.Views.Order
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

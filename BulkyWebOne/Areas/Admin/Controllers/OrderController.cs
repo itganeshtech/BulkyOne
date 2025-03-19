475940175e7512e78ded87f1cd8205effb1155af
itganeshtech <it.ganesh.tech@gmail.com>
2025-03-20 02:32:56 +0530

    Section 12 Order Processing Button Logic

--- BulkyWebOne/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWebOne/Areas/Admin/Controllers/OrderController.cs
@@ -70,17 +70,19 @@ namespace BulkyWebOne.Areas.Admin.Controllers
         {
             IEnumerable<OrderHeader> objOrderHeaders;
 
+
             if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
             {
                 objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
             }
             else
             {
+
                 var claimsIdentity = (ClaimsIdentity)User.Identity;
                 var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser");
-
+                objOrderHeaders = _unitOfWork.OrderHeader
+                    .GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser");
             }
             switch (status)
             {

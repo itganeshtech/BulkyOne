81292596b4303ff0cc40d198a46183a2dd5c6d98
itganeshtech <it.ganesh.tech@gmail.com>
2025-03-20 15:11:00 +0530

    Section 12 Ship Order

--- Bulky.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -11,5 +11,6 @@ namespace Bulky.DataAccess.Repository.IRepository
     {
         
        void Update(OrderHeader obj);
+        void UpdateStatus(int id, string orderStatus, string? paymentStatus = null);
     }
 }

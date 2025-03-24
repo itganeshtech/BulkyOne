7220c9778d6e6ec93303dd54b5ceb18f454a76e8
itganeshtech <it.ganesh.tech@gmail.com>
2025-03-24 10:34:11 +0530

    Section 14 DBInitializer Completed

--- BulkyWebOne/Program.cs
+++ BulkyWebOne/Program.cs
@@ -1,4 +1,5 @@
 using Bulky.DataAccess.Data;
+using Bulky.DataAccess.DBInitializer;
 using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Utility;
@@ -63,9 +64,18 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseSession();
+SeedDatabase();
 app.MapRazorPages();// Adding razor pages in the pipeline so that they can be accessed
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 app.Run();
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
